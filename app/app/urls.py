1cb555ea627eb73e4629cf255f7fc69edd33d685
ChritYabut <37975401+ChritYabut@users.noreply.github.com>
2023-11-07 18:42:04 +0800

    Update urls.py

--- app/app/urls.py
+++ app/app/urls.py
@@ -29,4 +29,6 @@ urlpatterns = [
         name='api-docs',
         ),
     path('api/user/', include('user.urls')),
+    path('api/timetask/', include('timetask.urls')),
+    path('api/project/', include('project.urls')),
 ]
